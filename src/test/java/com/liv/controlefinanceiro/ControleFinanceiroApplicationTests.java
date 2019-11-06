3e486e8d0700213e78080cb943c62587d9683da7
Andre Sales <andrelsales@gmail.com>
2019-11-06 10:08:07 -0300

    criando projeto

---
+++ src/test/java/com/liv/controlefinanceiro/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liv.controlefinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
