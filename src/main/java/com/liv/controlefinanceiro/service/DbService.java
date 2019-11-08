26d28fd3cc6e5fdeabd2b7bbb07ce0829f114fb3
Andre Sales <andrelsales@gmail.com>
2019-11-08 10:17:23 -0300

    enviando para producao

--- src/main/java/com/liv/controlefinanceiro/service/DbService.java
+++ src/main/java/com/liv/controlefinanceiro/service/DbService.java
@@ -26,17 +26,17 @@ public class DbService {
 	public void instantiateTesteDataBase() throws ParseException
 	{
 
-		TipoGasto tipo1 = new TipoGasto(null,"casa1",true);
-		TipoGasto tipo2 = new TipoGasto(null,"alimentacao2",true);
-		TipoGasto tipo3 = new TipoGasto(null,"diversão3",false);
+		TipoGasto tipo1 = new TipoGasto(null,"casa",true);
+		TipoGasto tipo2 = new TipoGasto(null,"alimentacao",true);
+		TipoGasto tipo3 = new TipoGasto(null,"diversão",false);
 		
-		TipoGasto tipo4 = new TipoGasto(null,"casa4",true);
-		TipoGasto tipo5 = new TipoGasto(null,"alimentacao5",true);
-		TipoGasto tipo6 = new TipoGasto(null,"diversão6",false);
+		TipoGasto tipo4 = new TipoGasto(null,"carro",true);
+		TipoGasto tipo5 = new TipoGasto(null,"saude",true);
+		TipoGasto tipo6 = new TipoGasto(null,"cachorro",false);
 		
-		TipoGasto tipo7 = new TipoGasto(null,"casa7",true);
-		TipoGasto tipo8 = new TipoGasto(null,"alimentacao8",true);
-		TipoGasto tipo9 = new TipoGasto(null,"diversão9",false);
+		TipoGasto tipo7 = new TipoGasto(null,"esporte",true);
+		TipoGasto tipo8 = new TipoGasto(null,"vestuário",true);
+		
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");		
 		
@@ -50,7 +50,7 @@ public class DbService {
 		GastoMes gt7 = new GastoMes(null,"tenis",12,2018,100,TipoPagamentoEnum.CARTAO_PARCELADO,3,3, tipo3,new Date(),sdf.parse("20/09/2018 10:32"));
 		
 				
-		tipoGastoRepository.saveAll(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo7,tipo8,tipo9));
+		tipoGastoRepository.saveAll(Arrays.asList(tipo1,tipo2,tipo3,tipo4,tipo5,tipo6,tipo7,tipo8));
 		
 		gastoMesRepository.saveAll(Arrays.asList(gt1,gt2,gt3,gt4,gt5,gt6,gt7));
 				
